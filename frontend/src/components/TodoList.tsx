17f0d2680e0c9d0061158b6a20aadaaabc7c5ae0
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-01 17:59:07 +0530

    add TodoList component for displaying a list of todo items

---
+++ frontend/src/components/TodoList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Todo } from '../types/Todo';
+import TodoItem from './TodoItem';
+
+interface TodoListProps {
+  todos: Todo[];
+  toggleTodo: (id: string) => Promise<void>;
+  deleteTodo: (id: string) => Promise<void>;
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos, toggleTodo, deleteTodo }) => {
+  if (todos.length === 0) {
+    return <div className="todo-empty">No todos yet. Add one above!</div>;
+  }
+
+  return (
+    <ul className="todo-list">
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
