7a9cafb2a1420df13c6260a4c991f36c5b84ffa3
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-09-01 18:44:03 +0530

    Refactor react-adapter to include useRef and KeyboardEvent type export

--- frontend/src/react-adapter.ts
+++ frontend/src/react-adapter.ts
@@ -5,8 +5,12 @@ import * as React from 'react';
 // Extend React with custom hooks
 export const useState = React.useState;
 export const useEffect = React.useEffect;
+export const useRef = React.useRef;
 export const createElement = React.createElement;
 export const Fragment = React.Fragment;
+
+// Types
+export type KeyboardEvent = React.KeyboardEvent<HTMLInputElement>;
 
 // Default export for React
 export default React;
